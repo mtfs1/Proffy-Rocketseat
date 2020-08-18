b3a36a613396e2ee60839d79c1d3ed9ddccd6dcc
Matheus Ferreira Santos <math.santos.2002@gmail.com>
2020-08-17 21:46:14 -0300

    redux persist implementation

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,15 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import {Provider} from "react-redux" 
-import store from "./redux/store"
+import {PersistGate} from "redux-persist/integration/react"
+import {store, persistor} from "./redux/store"
 
 import App from "./App"
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
